1036173187e31319fb3ab906766a9fce88f72afc
Juhi Park <juhpark@csumb.edu>
2019-01-29 01:27:54 +0000

    chaning php message

--- hello-world.php
+++ hello-world.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>Hello cloud9 & github</title>
-    </head>
-    <body>
-        you are here!
-    </body>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World PHP</p>'; ?> 
+ </body>
 </html>
