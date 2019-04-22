e18210a096ef8220e8bcdc42e928809ccdfc891f
Juhi Park <juhpark@csumb.edu>
2019-04-22 20:56:43 +0000

    Update graph

--- Practice/p10/api/dbConnect.php
+++ Practice/p10/api/dbConnect.php
@@ -2,13 +2,15 @@
     // heroku_d727c510ebe6dad
     // ottermart
     function getDBConnection($dbname = 'ottermart') {
-        // $host = 'us-cdbr-iron-east-03.cleardb.net'; //cloud 9 acting as host
-        // $username = 'b8282773fb41e0';
-        // $password = 'a78ad875';
+        $host = "us-cdbr-iron-east-03.cleardb.net"; //heroku
         
-        $host = 'localhost'; //cloud 9 acting as host
-        $username = 'root';
-        $password = '';
+        $username = "bb19a5ab5a5448";
+        $password = "0558428b";
+        $dbname = "heroku_88859229f24938c";
+        
+        // $host = 'localhost'; //cloud 9 acting as host
+        // $username = 'root';
+        // $password = '';
    
         //creates db connection
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
