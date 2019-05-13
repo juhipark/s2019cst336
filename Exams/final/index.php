6efc458a7fa45c2ab81c857441860db57726cb27
Juhi Park <juhpark@csumb.edu>
2019-05-13 18:35:30 +0000

    Final Fix

--- Exams/final/index.php
+++ Exams/final/index.php
@@ -162,13 +162,13 @@
 
         console.log("Currently Logged in user: ", currentUser);
         // populate the link
-        $("#invitationLinkInput").val('<?php 
+        $("#invitationLinkInput").val(`<?php 
                                             if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) { 
                                                 echo "https://juhipark-s2019cst336.herokuapp.com/Exams/final/invite.html?user=";
                                             }else {
                                                 echo "https://s2019cst336-juhipark.c9users.io/Exams/final/invite.html?user=";
                                             }
-                                         ?>' + currentUser);
+                                         ?>` + currentUser);
         // "https://s2019cst336-juhipark.c9users.io/Exams/final/invite.html?user=" + currentUser
         
         $( document ).ready(function() {
